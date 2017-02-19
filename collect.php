f43c09b625ea83ec5a04892424ce19cef18de605
Alice <albano_alice@yahoo.fr>
2017-02-19 15:59:34 +0100

    [DEV] Test if field t is in the URL

--- collect.php
+++ collect.php
@@ -1,6 +1,14 @@
 <?php
 
-foreach($_GET as $key => $item)
-{
-	echo $key." : ".$item."<br />\n";
+if (!array_key_exists("t", $_GET)){
+	header('HTTP/1.0 400 Bad Request');
+	echo "Error 400 Bad Request : field t missing\n";
+	exit;
+}else{
+
+	foreach($_GET as $key => $item)
+	{
+		echo $key." : ".$item."<br />\n";
+	}
 }
+
