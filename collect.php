3fd79c929d96becc0cca84244c66a3d722409fa8
Alice <albano_alice@yahoo.fr>
2017-02-19 17:58:14 +0100

    [DEV] Check value of mandatory parameters

--- collect.php
+++ collect.php
@@ -1,14 +1,19 @@
 <?php
 
+$t_values = array('pageview', 'screenview', 'event');
+$v_values = array(1);
+$tid_values = array('UA-XXXX-Y');
+
 $mandatory_fields = array(
-	't' => 'pageview',
-	'v' => 1,
-	'tid' => 'UA-XXXX-Y'
+	't' => $t_values,
+	'v' => $v_values,
+	'tid' => $tid_values
 	);
 
 foreach($mandatory_fields as $field => $value){
 
-	if (!array_key_exists($field, $_GET)){
+	if ((!array_key_exists($field, $_GET)) OR
+		(!in_array($_GET[$field], $value))){
 		header('HTTP/1.0 400 Bad Request');
 		echo "Error 400 Bad Request : field t missing\n";
 		exit;
