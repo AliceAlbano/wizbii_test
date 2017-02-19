fb04c4d7d98dbbe89d1b4b50e26606593e11ba09
Alice <albano_alice@yahoo.fr>
2017-02-19 19:10:47 +0100

    [REFACTOR] Split check paramater function in two
    One function checks the presence of mandatory parameters. The other checks the syntax of all given parameters

--- collect.php
+++ collect.php
@@ -1,5 +1,7 @@
 <?php
 
+//XXX: How can we know if it is a mobile hit ?
+//We don't implement these paramaters for now.
 $mandatory_fields = array(
 	't' => array('pageview', 'screenview', 'event'),
 	'v' => array(1),
@@ -7,19 +9,25 @@ $mandatory_fields = array(
 	'ds' => array('web', 'apps', 'backend'),
 	);
 
-function check_parameters($mandatory, $given) {
+function check_parameters_presence($mandatory, $given) {
 	foreach ($mandatory as $field => $value) {
 		if (!array_key_exists($field, $given))
-			return False;
-
-		if (!in_array($given[$field], $value))
 			return False;
 	}
 	return True;
 }
 
-$syntax_ok = check_parameters($mandatory_fields, $_GET);
-if (!$syntax_ok) {
+function check_parameters_syntax($expected, $given) {
+	foreach ($given as $field => $value) {
+		if (!in_array($value, $expected[$field]))
+			return False;
+	}
+	return True;
+}
+
+$presence_ok = check_parameters_presence($mandatory_fields, $_GET);
+$syntax_ok = check_parameters_syntax($mandatory_fields, $_GET);
+if (!$presence_ok OR !$syntax_ok) {
 	header('HTTP/1.0 400 Bad Request');
 	echo "Error 400 Bad Request : field t missing\n";
 	exit;
