2dbeba3d6f12dcba22c0ebb1d9230528646a6570
Alice <albano_alice@yahoo.fr>
2017-02-19 23:58:55 +0100

    [DEV] Use MeasureChecker to verify the validity of a measure

--- collect.php
+++ collect.php
@@ -1,15 +1,8 @@
 <?php
 
-require 'MeasureDescription.php';
+require 'MeasureChecker.php';
 
 $measure = new MeasureDescription;
+$measureChecker = new MeasureChecker;
 
-$existence_ok = $measure->existing_parameters($_GET);
-$syntax_ok = $measure->valid_parameters($_GET);
-$mandatory_ok = $measure->mandatory_parameters($_GET);
-
-if (!$existence_ok OR !$mandatory_ok OR !$syntax_ok) {
-	header('HTTP/1.0 400 Bad Request');
-	echo "Error 400 Bad Request\n";
-	exit;
-}
+$measureChecker->valid_description($measure);
