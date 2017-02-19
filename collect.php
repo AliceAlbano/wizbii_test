9169bd923a2d2f934ea779b4dd16369190492ee6
Alice <albano_alice@yahoo.fr>
2017-02-19 18:45:10 +0100

    [REFACTOR] Simplify check_paramaters function
    Split the check condition in two

--- collect.php
+++ collect.php
@@ -6,18 +6,19 @@ $mandatory_fields = array(
 	'tid' => array('UA-XXXX-Y')
 	);
 
-function check_parameters($mandatory, $given){
-	foreach ($mandatory as $field => $value){
-		if ((!array_key_exists($field, $given)) OR
-			(!in_array($given[$field], $value))){
+function check_parameters($mandatory, $given) {
+	foreach ($mandatory as $field => $value) {
+		if (!array_key_exists($field, $given))
 			return False;
-		}
+
+		if (!in_array($given[$field], $value))
+			return False;
 	}
 	return True;
 }
 
 $syntax_ok = check_parameters($mandatory_fields, $_GET);
-if (!$syntax_ok){
+if (!$syntax_ok) {
 	header('HTTP/1.0 400 Bad Request');
 	echo "Error 400 Bad Request : field t missing\n";
 	exit;
