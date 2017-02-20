88d6a75b11255d3aef3570822668bbb9e71b4433
Alice <albano_alice@yahoo.fr>
2017-02-20 12:17:20 +0100

    [TYPO] Remove old echos

--- collect.php
+++ collect.php
@@ -7,7 +7,6 @@ $validMeasure = new MeasureChecker($_GET);
 
 $validMeasure->valid_description($measure);
 if ($validMeasure) {
-	echo "On va inserer la requete";
 	$stockrequest = new StockRequest($_GET);
 	$stockrequest->insertRequest();
 }
